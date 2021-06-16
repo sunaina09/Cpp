df374d2835567b6bda7f4d664c695f8810368050
Sunaina <81823428+sunaina09@users.noreply.github.com>
2021-06-16 20:07:28 +0530

    Add files via upload

---
+++ references.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ string food= "Pizza";
+ string & meal= food;
+
+ cout << food<< endl;
+ cout <<meal<<endl;
+ cout <<&meal<<endl;
+
+
+
+ return 0;
+}
